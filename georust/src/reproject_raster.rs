ab9e3ebeff480209c4bfff28fd39f02686d95560
Roger Almengor Gonzalez <roger.almengor@gaf.de>
2023-05-26 07:18:03 +0200

    refactor: uses gdal.

--- georust/src/reproject_raster.rs
+++ georust/src/reproject_raster.rs
@@ -1,62 +1,50 @@
-use std::env;
-use std::io::{self, Error, ErrorKind};
-use proj::Proj;
-extern crate proj::Proj;
+extern crate gdal;
 
-pub fn reproject_raster(input: &[Vec<f64>], src_proj: &str, dst_proj: &str) -> Result<Vec<Vec<f64>>, Error> {
-    // Get the transformation object for the source and destination projections
-    let src = Proj::new(src_proj)?;
-    let dst = Proj::new(dst_proj)?;
+use gdal::raster::{Dataset, Driver};
+use gdal::spatial_ref::SpatialRef;
 
-    // Reproject each pixel in the input raster
-    let mut output = Vec::new();
-    for row in input {
-        let mut output_row = Vec::new();
-        for &value in row {
-            // Convert the coordinates and store the reprojected value
-            let (x, y) = src
-                .proj(&proj::Point::new(value, value))?
-                .to_tuple();
-            let reprojected_value = dst
-                .unproj(&proj::Point::new(x, y))?
-                .x;
-            output_row.push(reprojected_value);
-        }
-        output.push(output_row);
+fn reproject_raster(input_path: &str, output_path: &str, target_epsg: &str) -> Result<(), String> {
+    // Open the input raster dataset
+    let dataset = Dataset::open(input_path).map_err(|e| e.to_string())?;
+
+    // Get the input raster's projection
+    let source_srs = dataset.projection();
+
+    // Create a SpatialRef for the target EPSG code
+    let mut target_srs = SpatialRef::new();
+    target_srs.set_from_epsg(target_epsg.parse().unwrap()).map_err(|e| e.to_string())?;
+
+    // Create a new Dataset for the output raster
+    let driver = Driver::get("GTiff").map_err(|e| e.to_string())?;
+    let mut output_dataset = driver.create(output_path, dataset.raster_size(), 1, dataset.band_count(), dataset.band_type())
+        .map_err(|e| e.to_string())?;
+
+    // Set the output dataset's projection to the target EPSG
+    output_dataset.set_projection(&target_srs.to_wkt()).map_err(|e| e.to_string())?;
+
+    // Loop over the bands and reproject each one
+    for i in 1..=dataset.band_count() {
+        let band = dataset.band(i).unwrap();
+        let mut output_band = output_dataset.band(i).unwrap();
+
+        // Reproject the band
+        band.reproject_to(&mut output_band, &source_srs, &target_srs)
+            .map_err(|e| e.to_string())?;
     }
 
-    Ok(output)
+    Ok(())
 }
 
 fn main() {
-    // Get the command-line arguments
-    let args: Vec<String> = env::args().collect();
+    let input_path = "path/to/input/raster.tif";
+    let output_path = "path/to/output/raster.tif";
+    let target_epsg = "4326";  // Example: EPSG code for WGS84
 
-    // Check if a raster path is provided
-    if args.len() < 2 {
-        eprintln!("Please enter a raster path.");
-        return;
-    }
-
-    // Extract the raster path from the command-line arguments
-    let raster_path = &args[1];
-
-    // Example usage
-    let input: Vec<Vec<f64>> = vec![vec![1.0, 2.0], vec![3.0, 4.0]];
-    let src_proj = "+proj=longlat +datum=WGS84 +no_defs";
-    let dst_proj = "+proj=utm +zone=32 +datum=WGS84 +units=m +no_defs";
-
-    match reproject_raster(&input, src_proj, dst_proj) {
-        Ok(reprojected) => {
-            println!("Reprojection successful:");
-            for row in reprojected {
-                for value in row {
-                    print!("{} ", value);
-                }
-                println!();
-            }
-        }
-        Err(err) => eprintln!("Reprojection error: {}", err),
+    if let Err(err) = reproject_raster(input_path, output_path, target_epsg) {
+        eprintln!("Error: {}", err);
+    } else {
+        println!("Raster reprojection complete.");
     }
 }
+
 
