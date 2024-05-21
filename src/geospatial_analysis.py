1d666b2f0f1464e555e657ebb54ac613e1221b3f
Roger Almengor <32868863+rogeralmengor@users.noreply.github.com>
2024-05-21 09:36:35 +0000

    chore: remove unused imports.

--- src/geospatial_analysis.py
+++ src/geospatial_analysis.py
@@ -1,6 +1,4 @@
 """Gives a range of functions and classes to work with raster and vector data."""
-import numpy as np 
-import geopandas as gpd
 
 def hello_user(user:str)->None:
     """Say hello to a user"""
