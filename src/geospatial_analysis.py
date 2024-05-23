c536ddc07693773ed942babdc582bfc5544f45d9
Roger Almengor <32868863+rogeralmengor@users.noreply.github.com>
2024-05-23 12:46:31 +0000

    chore: apply black formatter.

--- src/geospatial_analysis.py
+++ src/geospatial_analysis.py
@@ -1,5 +1,6 @@
 """Gives a range of functions and classes to work with raster and vector data."""
 
-def hello_user(user:str)->None:
+
+def hello_user(user: str) -> None:
     """Say hello to a user"""
     print(f"Hello {user}!")
--- src/test_src.py
+++ src/test_src.py
@@ -1,4 +1,5 @@
 from . import geospatial_analysis
 
+
 def test_hello_user():
     geospatial_analysis.hello_user("Roger")
