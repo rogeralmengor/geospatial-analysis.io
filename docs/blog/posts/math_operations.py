d7c192859d9a34419a961a18bd3e36cb31f148fd
Roger Almengor <32868863+rogeralmengor@users.noreply.github.com>
2024-08-26 09:15:53 +0000

    feat: adding module basic arithmetic operations.

---
+++ docs/blog/posts/math_operations.py
@@ -0,0 +1,15 @@
+def add(a: float, b: float) -> float:
+    """Add two numbers."""
+    return a + b
+
+def subtract(a: float, b: float) -> float:
+    """Subtract b from a."""
+    return a - b
+
+def divide(a: float, b: float) -> float:
+    """Divide a by b."""
+    return a / b
+
+def multiply(a: float, b: float) -> float:
+    """Multiply a and b."""
+    return a * b
