90397c0fbefc2abee1a7c2bf05aacf2dcdf01f6a
Roger Almengor <32868863+rogeralmengor@users.noreply.github.com>
2024-10-21 06:43:59 +0000

    add portfolio Layout.

---
+++ docs/portfolio/Languages/script.js
@@ -0,0 +1,5 @@
+function openDocumentation(projectUrl) {
+    // Assuming your MkDocs-generated documentation is hosted in 'site/projectX/'
+    // Replace 'projectUrl' with the actual path to the HTML page rendered by MkDocs
+    window.location.href = projectUrl;
+}
